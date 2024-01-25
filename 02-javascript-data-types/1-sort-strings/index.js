bcda8ddd08f7a0e8def79957c0b506160555855e
Oleksandr Mykhailovskiy <aleksejmikhajlovs@gmail.com>
2024-01-25 17:18:08 +0200

    feat: lesson 1 HW

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,13 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    const resultArray = arr.slice();
 
+    if(param === 'desc'){
+        resultArray.sort( (a, b) => b.localeCompare(a, ["ru", "en"], {caseFirst: 'upper'}) );
+    }else{
+        resultArray.sort( (a, b) => a.localeCompare(b, ["ru", "en"], {caseFirst: 'upper'}) );
+    }
+    
+    return resultArray;
 }
